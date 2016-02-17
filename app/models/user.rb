ae393b75613db0c9d02ad50264217945dfc90a45
zhang <1417278284@qq.com>
2016-02-17 20:49:04 +0800

    add appstore test

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,38 @@ class User < ActiveRecord::Base
 
   def self.sign_in(phone_num_encrypt,rand_code)
     redis_rand_code = $redis.get(phone_num_encrypt)
-    if redis_rand_code == rand_code
-      #1.验证正确,存入cookies.
-      token = SecureRandom.urlsafe_base64
-      user = User.find_by(phone_num:phone_num_encrypt)
-      if user.present?
-        user.update(token:token)
+    token = nil
+    unique_id = nil
+    if phone_num_encrypt == "F59E10256A72D10742349BEBBFDD8FA8"
+      if rand_code == "1111"
+        #1.验证正确,存入cookies.
+        token = SecureRandom.urlsafe_base64
+        user = User.find_by(phone_num:phone_num_encrypt)
+        if user.present?
+          user.update(token:token)
+        else
+          user = User.create(token:token,phone_num:phone_num_encrypt,unique_id:SecureRandom.urlsafe_base64,rand:"铜")
+        end
+        unique_id = user.unique_id
       else
-        user = User.create(token:token,phone_num:phone_num_encrypt,unique_id:SecureRandom.urlsafe_base64,rand:"铜")
+        token = nil
       end
-      unique_id = user.unique_id
     else
-      token = nil
+      if redis_rand_code == rand_code
+        #1.验证正确,存入cookies.
+        token = SecureRandom.urlsafe_base64
+        user = User.find_by(phone_num:phone_num_encrypt)
+        if user.present?
+          user.update(token:token)
+        else
+          user = User.create(token:token,phone_num:phone_num_encrypt,unique_id:SecureRandom.urlsafe_base64,rand:"铜")
+        end
+        unique_id = user.unique_id
+      else
+        token = nil
+      end
     end
+
     [token,unique_id]
   end
 end
